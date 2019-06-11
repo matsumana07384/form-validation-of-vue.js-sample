e094a05cd068d40fcae6c67f02904f6c019db0ae
matsumana07384 <manami.m.227@outlook.jp>
2019-06-11 13:39:22 +0900

    organizing of file

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <SampleData></SampleData>
-    <!-- <Form title="ID" formType="text"></Form>
-    <Form title="Password" formType="password"></Form>
-    <Button buttonLabel="確認する"></Button>
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import Form from './components/form.vue'
-import Button from './components/button.vue'
-import SampleData from './components/form-sample.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
-    Form,
-    Button,
-    SampleData
-  },
-  methods:{
-
+    HelloWorld
   }
 }
 </script>
