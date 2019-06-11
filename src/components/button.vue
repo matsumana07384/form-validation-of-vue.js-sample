e0b147be17e88f09026565152612d9b82dc2fd2f
matsumana07384 <manami.m.227@outlook.jp>
2019-06-11 14:31:05 +0900

    add components

---
+++ src/components/button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button>{{buttonLabel }}</button>
+</template>
+<script>
+export default {
+  props: {
+    'buttonLabel' :{
+      type: String,
+      default: 'send',
+      required: true
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
