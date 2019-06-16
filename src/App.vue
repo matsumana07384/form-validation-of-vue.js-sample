345b0dc6e6397fdef880a27d6f2769accd44eed7
matsumana07384 <manami.m.227@outlook.jp>
2019-06-16 13:55:27 +0900

    フォーム入力バインディングを追加

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <SampleData></SampleData>
-    <!-- <Form title="ID" formType="text"></Form>
-    <Form title="Password" formType="password"></Form>
-    <Button buttonLabel="確認する"></Button>
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <hr>
+    <div class="form">
+      <form>
+        <span>ID : </span>          
+          <input
+            type="text"
+            placeholder="input your id"
+            v-model="loginId"
+            >
+      </form>
+        <p> loginId is {{ loginId }} </p>
+      <form>
+        <span>Password: </span>
+          <input 
+            type="password"
+            placeholder="edit me"
+            v-model="passWWord"
+          >
+      </form>
+        <p>Input Password is {{passWWord}}</p>
+    </div>
+    <hr>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import Form from './components/form.vue'
-import Button from './components/button.vue'
-import SampleData from './components/form-sample.vue'
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
-    Form,
-    Button,
-    SampleData
   },
-  methods:{
-
+  data: function() { 
+    return {
+      //値の初期化
+      loginId:'',
+      passWWord:''
+    }
   }
 }
 </script>
